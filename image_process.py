7b3e4a3e9d8086c1883adf93abff44e86cc547b4
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-04-17 01:30:35 +0530

    Convex hull added with defects

--- image_process.py
+++ image_process.py
@@ -3,18 +3,18 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 areaglobal=0
+oldlen=0
 permieter=0
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     # Our operations on the frame come her
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ret,thresh = cv2.threshold(gray,130,255,0)
-    ret2,thresh2 = cv2.threshold(gray,130,180,0)
+    ret,thresh = cv2.threshold(gray,90,255,cv2.THRESH_BINARY_INV)
+    ret2,thresh2 = cv2.threshold(gray,90,255,cv2.THRESH_BINARY_INV)
     contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     # Display the resulting frame
     #cnt=contours[0]
-    global cnt
     for i in contours:
     	area=cv2.contourArea(i)
     	if area>1000:
@@ -25,11 +25,24 @@ while(True):
         areaglobal=cv2.contourArea(cnt)
         perimeter=cv2.arcLength(cnt,True)
     #cv2.drawContours(frame, [cnt], 0, (255,0,0), 2)
-    approx=cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-    cv2.polylines(frame,[approx],True,(255,0,0))
+    approx=cv2.convexHull(cnt,returnPoints=False)
+    defects=cv2.convexityDefects(cnt,approx)
+    #print defects
+    if len(approx)!=oldlen:
+        #print len(approx)
+        oldlen=len(approx)
+    print len(range(defects.shape[0]))
+    for i in range(defects.shape[0]):
+        s,e,f,d = defects[i,0]
+        start = tuple(cnt[s][0])
+        end = tuple(cnt[e][0])
+        far = tuple(cnt[f][0])
+        cv2.line(frame,start,end,[0,255,0],2)
+        cv2.circle(frame,far,5,[0,0,255],-1)
+    #cv2.polylines(frame,[approx],True,(255,0,0))
     #cv2.imshow('frame',thresh)
     cv2.imshow('image',frame)
-    #cv2.imshow('thresh',thresh2)
+    cv2.imshow('thresh',thresh2)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
     	
